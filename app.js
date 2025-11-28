011f797c1599354d550a6eb07ee43f594c03100d
cicero <ciciro4554@gmail.com>
2025-11-28 17:11:32 -0300

    endereço

---
+++ app.js
@@ -0,0 +1,35 @@
+const endereço = {
+rua: document.querySelector('#rua'),
+bairro : document.querySelector('#bairro'),
+cidade : document.querySelector('#cidade'),
+estado : document.querySelector('#estado')
+
+}
+const  cep ="06520080";
+
+async function consultaCEP(cep){
+
+const url = await fetch (`https://viacep.com.br/ws/${cep}/json/`)
+
+const resposta = await url.json();
+
+let cepUsuario = {
+
+rua: resposta.logradouro,
+bairro: resposta.bairro,
+cidade: resposta.localidade,
+estado: resposta.uf
+
+}
+
+rua.innerText= cepUsuario.rua
+bairro.innerText= cepUsuario.bairro
+cidade.innerText= cepUsuario.cidade
+estado.innerText= cepUsuario.estado
+
+console.log(cepUsuario)
+
+}
+
+consultaCEP(cep)
+
